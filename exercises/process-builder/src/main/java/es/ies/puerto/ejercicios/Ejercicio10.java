7f698c71a39739b7154d7d6fdfc0f4b06273b7cf
mackstm <thelewyntm@gmail.com>
2024-09-25 19:06:13 +0100

    a

--- exercises/process-builder/src/main/java/es/ies/puerto/ejercicios/Ejercicio10.java
+++ exercises/process-builder/src/main/java/es/ies/puerto/ejercicios/Ejercicio10.java
@@ -1,7 +1,41 @@
 package es.ies.puerto.ejercicios;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 public class Ejercicio10 {
     public static void main(String[] args) {
-        ProcessBuilder pbLs = new ProcessBuilder("ls");
+        ProcessBuilder pbLs = new ProcessBuilder("ls", "-l");
+        ProcessBuilder pbGrep = new ProcessBuilder("grep", "README");
+
+        try {
+            Process lsProcess = pbLs.start();
+            
+            InputStream lsIn = lsProcess.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(lsIn));
+
+            String lsOutput = "";
+
+            while (br.readLine()!= null) {
+                lsOutput += br.readLine() + "\n";
+            }
+            
+            int lsExit = lsProcess.waitFor();
+
+            Process grepProcess = pbGrep.start();
+
+            OutputStream grepOut = grepProcess.getOutputStream();
+
+            int outputExit = grepProcess.waitFor();
+        
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
