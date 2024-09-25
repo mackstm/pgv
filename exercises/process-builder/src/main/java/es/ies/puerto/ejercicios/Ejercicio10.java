d7b35af37d229de9175187db797f9d88f1e028c5
mackstm <thelewyntm@gmail.com>
2024-09-25 17:15:06 +0100

    a

--- exercises/process-builder/src/main/java/es/ies/puerto/ejercicios/Ejercicio10.java
+++ exercises/process-builder/src/main/java/es/ies/puerto/ejercicios/Ejercicio10.java
@@ -2,6 +2,6 @@ package es.ies.puerto.ejercicios;
 
 public class Ejercicio10 {
     public static void main(String[] args) {
-        
+        ProcessBuilder pbLs = new ProcessBuilder("ls");
     }
 }
